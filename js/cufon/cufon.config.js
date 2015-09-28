f6bf87ecc5b8a9ebe580b9d8d9104ae146d66ab7
demmyboy <demolaabdulai@live.com>
2015-09-28 20:58:17 +0200

    this is my first commit

---
+++ js/cufon/cufon.config.js
@@ -0,0 +1,24 @@
+/* 
+  Cufon Config
+    
+*/			
+
+Cufon.replace('h1', { fontFamily: 'museo', hover: true, textShadow: '#604837 0px 1px ' });
+Cufon.replace('.header .lead h1 ', { fontFamily: 'museo', hover: true, textShadow: '#6d9d98 0px 1px ' });
+Cufon.replace('.main h1 ', { fontFamily: 'museo', hover: true, textShadow: '#e0dece 0px 1px ' });
+
+
+Cufon.replace('h2', { fontFamily: 'museo', hover: true });
+Cufon.replace('h3', { fontFamily: 'museo', hover: true });
+Cufon.replace('.footer h3', { fontFamily: 'museo', hover: true, textShadow: '#6d523f 0px 1px ' });
+
+
+Cufon.replace('h4', { fontFamily: 'museo', hover: true });
+
+/* 
+
+*/
+
+Cufon.replace('.header .lower ul li a', { fontFamily: 'museo', hover: true, textShadow: '#fff 0px 1px ' });
+Cufon.replace('form label', { fontFamily: 'museo', hover: true  });
+Cufon.replace('.main .portion-table ul.split li.first', { fontFamily: 'museo', hover: true  });
---
+++ js/jqueries.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  
+  // set up lightboxes
+  $('a.lbox, a.cbox').lightBox();
+
+  $('#home_slider').bxCarousel({
+    display_num: 4,
+    move: 1,
+    margin: 20
+  });
+  
+  $('input, textarea').bind('focus', function(){
+    $(this).addClass('focused');
+  });
+  $('input, textarea').bind('blur', function(){
+    $(this).removeClass('focused');
+  });
+  
+});
